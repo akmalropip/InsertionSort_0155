caf1f413f05f758ad875d1870026c1d79f742ebb
akmalropip <akmalropip@gmail.com>
2026-03-13 15:21:13 +0700

    membuat prosedur insertion sort

--- file1.cpp
+++ file1.cpp
@@ -27,3 +27,24 @@ for (int i = 0; i < n; i++ ){
 }
 }
 
+void insertionsort(){ //prosedur insertion sort
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++){
+        temp = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
+
