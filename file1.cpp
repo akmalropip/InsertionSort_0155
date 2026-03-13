2bfc16bbcd425e0ac27926feb36720d13a973a58
akmalropip <akmalropip@gmail.com>
2026-03-13 15:26:14 +0700

    menambahkan void display

--- file1.cpp
+++ file1.cpp
@@ -48,3 +48,14 @@ void insertionsort(){ //prosedur insertion sort
     }
 }
 
+void display(){
+    cout << endl;
+    cout << "\n=============";
+    cout << "\nElement Array" << endl;
+    cout << "\n=============";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
