ed1e37265953fc4fd6e6e454156a27a707bf356b
akmalropip <akmalropip@gmail.com>
2026-03-13 15:11:29 +0700

    membuat prosedur input

--- file1.cpp
+++ file1.cpp
@@ -3,3 +3,27 @@ using namespace std;
 
 int arr[20]; //membuat array dengan panjang data 20
 int n; //membuat variabel inputan n
+
+void input (){ //prosedur input
+while (true){
+    cout << "Masukkan Jumlah Data Pada Array : ";
+    cin >> n;
+
+    if (n <= 20){
+        break;
+    }
+    else{
+        cout << "\nArray Tidak Boleh Lebih dari 20\n";
+    }
+}
+cout << endl;
+cout << "==========================================";
+cout << "==========Masukkan Element Array==========";
+cout << "==========================================";
+
+for (int i = 0; i < n; i++ ){
+    cout << "Data ke-" << (i+1) << ": ";
+    cin >> arr[i];
+}
+}
+
